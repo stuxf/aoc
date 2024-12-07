c63df0c1f1d461bd49f5c6f458574c020fa867dd
user <70670632+stuxf@users.noreply.github.com>
2024-12-06 21:48:01 -0800

    🐍 flocto is the goat

--- 2024/07/07.py
+++ 2024/07/07.py
@@ -5,23 +5,40 @@ calibrations = []
 num_valid = 0
 num_valid_tolerances = 0
 
-def all_valid(arr_nums):
-    if len(arr_nums) == 1:
-        return [arr_nums[0]]
-    return [arr_nums[0] * num for num in all_valid(arr_nums[1:])] + [arr_nums[0] + num for num in all_valid(arr_nums[1:])] + [int(str(num) + str(arr_nums[0])) for num in all_valid(arr_nums[1:])]
 
-def all_valid_two(arr_nums):
+def all_valid(arr_nums, res):
     if len(arr_nums) == 1:
         return [arr_nums[0]]
-    return [arr_nums[0] * num for num in all_valid(arr_nums[1:])] + [arr_nums[0] + num for num in all_valid(arr_nums[1:])] + [int(str(num) + str(arr_nums[0])) for num in all_valid(arr_nums[1:])]
+    return [
+        s
+        for num in all_valid(arr_nums[1:], res)
+        for s in (arr_nums[0] + num, arr_nums[0] * num)
+        if s <= res
+    ]
+
+
+def all_valid_two(arr_nums, res):
+    if len(arr_nums) == 1:
+        return [arr_nums[0]]
+    return [
+        s
+        for num in all_valid_two(arr_nums[1:], res)
+        for s in (
+            arr_nums[0] + num,
+            arr_nums[0] * num,
+            int(str(num) + str(arr_nums[0])),
+        )
+        if s <= res
+    ]
+
 
 for line in data:
     a = int(line.split(":")[0])
     b = [*map(int, line.split(":")[1].strip().split())]
     calibrations.append((a, b))
-    if a in all_valid(b[::-1]):
+    if a in all_valid(b[::-1], a):
         num_valid += a
-    elif a in all_valid_two(b[::-1]):
+    elif a in all_valid_two(b[::-1], a):
         num_valid_tolerances += a
 
 print(num_valid, num_valid_tolerances)
