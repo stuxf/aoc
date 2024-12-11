9285c10252e1a09e802f521f6890ec41dd690334
user <70670632+stuxf@users.noreply.github.com>
2024-12-10 22:00:19 -0800

    🐍 day 11, not too bad

---
+++ 2024/11/11.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+data = [*map(int, open("input").read().splitlines()[0].split())]
+
+stones = defaultdict(int)
+
+for stone in data:
+    stones[stone] += 1
+
+def blink(stones):
+    new_stones = defaultdict(int)
+    for stone, count in stones.items():
+        stone_l = len(str(stone))
+        if stone == 0:
+            new_stones[1] += count
+        elif stone_l % 2 == 0:
+            new_stones[int(str(stone)[:stone_l//2])] += count
+            new_stones[int(str(stone)[stone_l//2:])] += count
+        else:
+            new_stones[stone * 2024] += count
+    return new_stones
+
+for blinks in range(75):
+    if blinks == 25:
+        print(sum(stones.values()))
+    stones = blink(stones)
+
+print(sum(stones.values()))
