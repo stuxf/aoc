fc5f4ee9047d45a36699e9587fd6b1a9835dc8df
user <70670632+stuxf@users.noreply.github.com>
2024-12-19 14:43:47 -0800

    🐍 faster, but still could be better

--- 2024/Python/06.py
+++ 2024/Python/06.py
@@ -1,80 +1,60 @@
-from collections import defaultdict
+data = [list(line) for line in open(f"../inputs/06.txt").read().splitlines()]
 
-data = [*map(list, open("../inputs/06.txt").read().splitlines())]
+H, W = len(data), len(data[0])
 
-for y, row in enumerate(data):
-    for x, item in enumerate(row):
-        if item == "^":
-            start = (y, x)
+start = next(
+    (y, x) for y, row in enumerate(data) for x, cell in enumerate(row) if cell == "^"
+)
 
-assert data[start[0]][start[1]] == "^"
-
-visited = set()
-
-visited.add(start)
-
-direction = (-1, 0)
+dirs = [(1, 0), (0, -1), (-1, 0), (0, 1)]
 
 
-def turn(tuple):
-    match tuple:
-        case (1, 0):
-            return (0, -1)
-        case (0, 1):
-            return (1, 0)
-        case (-1, 0):
-            return (0, 1)
-        case (0, -1):
-            return (-1, 0)
-        case _:
-            return tuple
+def turn(d):
+    return dirs[(dirs.index(d) + 1) % 4]
 
 
-def update_pos(pos, direction):
-    return (pos[0] + direction[0], direction[1] + pos[1])
+def in_bounds(y, x):
+    return 0 <= y < H and 0 <= x < W
 
 
-def in_bounds(tuple):
-    return 0 <= x < len(data[0]) and 0 <= y < len(data)
-
-
-y, x = start[0], start[1]
-
-while True:
-    y, x = update_pos((y, x), direction)
-    if not in_bounds((y, x)):
-        break
-    if data[y][x] == "#":
-        y, x = update_pos((y, x), (direction[0] * -1, direction[1] * -1))
-        direction = turn(direction)
-    visited.add((y, x))
-
-print(len(visited))
-
-
-def hasLoop(o_y, o_x):
-    new_visited = set()
-    direction = (-1, 0)
-    y, x = start[0], start[1]
-    new_visited.add((y, x, direction))
+def walk():
+    visited = set()
+    dy, dx = (-1, 0)
+    y, x = start
     while True:
-        y, x = update_pos((y, x), direction)
-        if (y, x, direction) in new_visited:
-            return True
-        if not (0 <= x < len(data[0]) and 0 <= y < len(data)):
+        y, x = y + dy, x + dx
+        if not in_bounds(y, x):
             break
-        if data[y][x] == "#" or (y == o_y and x == o_x):
-            y, x = update_pos((y, x), (direction[0] * -1, direction[1] * -1))
-            direction = turn(direction)
-        new_visited.add((y, x, direction))
-    return False
+        if data[y][x] == "#":
+            y, x = y - dy, x - dx
+            dy, dx = turn((dy, dx))
+        visited.add((y, x))
+    return visited
 
+def has_loop(o_y, o_x, grid_copy):
+    visited = set()
+    dy, dx = (-1, 0)
+    y, x = start
+    visited.add((y, x, dy, dx))
+    while True:
+        y, x = y + dy, x + dx
+        if (y, x, dy, dx) in visited:
+            return True
+        if not in_bounds(y, x):
+            return False
+        if grid_copy[y][x] == "#" or (y == o_y and x == o_x):
+            y, x = y - dy, x - dx
+            dy, dx = turn((dy, dx))
+        visited.add((y, x, dy, dx))
+
+path = walk()
+
+print(len(path))
 
 print(
     sum(
-        hasLoop(o_y, o_x)
-        for o_y in range(len(data))
-        for o_x in range(len(data[0]))
+        has_loop(o_y, o_x, [row[:] for row in data])
+        for o_y, o_x in path
         if data[o_y][o_x] == "."
     )
 )
